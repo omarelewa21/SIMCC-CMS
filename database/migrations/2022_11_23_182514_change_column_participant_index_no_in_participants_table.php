2a78818d6789c057c405f3af18b1bf0348e8844a
Hashtechs <hashtechs.stack@gmail.com>
2022-11-29 20:27:06 +0200

    change column collate

---
+++ database/migrations/2022_11_23_182514_change_column_participant_index_no_in_participants_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('participants', function (Blueprint $table) {
+            $table->string('index_no')->collation('utf8mb4_general_ci')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('participants', function (Blueprint $table) {
+            $table->string('index_no')->collation('utf8mb4_general_ci')->change();
+        });
+    }
+};
