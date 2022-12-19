642a2ecd2a1fd75905c013a09b8bbf87299226d2
--global <--global>
2022-12-19 15:11:11 +0200

    edit validation for awards

--- app/Http/Controllers/Api/MarkingController.php
+++ app/Http/Controllers/Api/MarkingController.php
@@ -348,19 +348,20 @@ class MarkingController extends Controller
                 ->join('organization', 'organization.id', 'competition_organization.organization_id')
                 ->select(
                     DB::raw("CONCAT('\"', competition.name, '\"') AS competition"),
-                DB::raw("CONCAT('\"', organization.name, '\"') AS organization"),
-                DB::raw("CONCAT('\"', all_countries.display_name, '\"') AS country"),
-                DB::raw("CONCAT('\"', competition_levels.name, '\"') AS level"),
-                'participants.grade',
-                DB::raw("CONCAT('\"', schools.name, '\"') AS school"),
-                'participants.index_no as index',
-                DB::raw("CONCAT('\"', participants.name, '\"') AS participant"),
-                'competition_participants_results.points',
-                DB::raw("CONCAT('\"', competition_participants_results.award, '\"') AS award"),
-                'competition_participants_results.school_rank',
-                'competition_participants_results.country_rank',
-                'competition_participants_results.global_rank'
-                )->distinct('index')->orderBy('points', 'DESC')->get();
+                    DB::raw("CONCAT('\"', organization.name, '\"') AS organization"),
+                    DB::raw("CONCAT('\"', all_countries.display_name, '\"') AS country"),
+                    DB::raw("CONCAT('\"', competition_levels.name, '\"') AS level"),
+                    'participants.grade',
+                    DB::raw("CONCAT('\"', schools.name, '\"') AS school"),
+                    'participants.index_no as index',
+                    DB::raw("CONCAT('\"', participants.name, '\"') AS participant"),
+                    'competition_participants_results.points',
+                    DB::raw("CONCAT('\"', competition_participants_results.award, '\"') AS award"),
+                    'competition_participants_results.school_rank',
+                    'competition_participants_results.country_rank',
+                    'competition_participants_results.global_rank'
+                )
+                ->distinct('index')->orderBy('points', 'DESC')->get();
 
             return $data;
         }
@@ -412,7 +413,7 @@ class MarkingController extends Controller
         try {
             foreach($request->all() as $data){
                 $participantResults = $level->participantResults()->where('competition_participants_results.participant_index', $data['participant_index'])
-                    ->first();
+                    ->firstOrFail();
 
                 $participantResults->update([
                     'award'         => $data['award'],
