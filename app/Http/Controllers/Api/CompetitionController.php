350ff29a8fa860fb21a00425675649e58bfefeea
Omar Elewa <omarelewa25@gmail.com>
2023-02-10 10:48:24 +0200

    ---

--- app/Http/Controllers/Api/CompetitionController.php
+++ app/Http/Controllers/Api/CompetitionController.php
@@ -725,7 +725,7 @@ class CompetitionController extends Controller
         }
     }
 
-    private function addOrganization(array $organizations, int $competition_id)
+    public function addOrganization(array $organizations, int $competition_id)
     {
         foreach($organizations as $organization){
             CompetitionOrganization::create(
