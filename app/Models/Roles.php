c14e52be6d3e37abdd986c3a542e24771bfb102c
Omar Elewa <omarelewa25@gmail.com>
2023-03-30 05:52:08 +0200

    fix error

--- app/Models/Roles.php
+++ app/Models/Roles.php
@@ -9,6 +9,13 @@ class Roles extends Model
 {
     use HasFactory;
 
+    CONST SUPER_ADMIN_ID = 0;
+    CONST ADMIN_ID = 1;
+    CONST COUNTRY_PARTNER_ID = 2;
+    CONST TEACHER_ID = 3;
+    CONST COUNTRY_PARTNER_ASSISTANT_ID = 4;
+    CONST SCHOOL_MANAGER = 5;
+
     protected $table = 'roles';
     protected $fillable = [
         'parent_id',
