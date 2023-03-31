3db9815391a3e62e9020d8d343c29f41ccfec20a
Omar Elewa <omarelewa25@gmail.com>
2023-03-31 08:36:24 +0200

    --

--- app/Rules/CheckParticipantDeleteExpire.php
+++ app/Rules/CheckParticipantDeleteExpire.php
@@ -34,10 +34,10 @@ class CheckParticipantDeleteExpire implements InvokableRule
         if(Auth()->user()->hasRole('Super Admin', 'Admin')) return;
 
         $participant = Participants::where('id', $value)->first();
-        $restrictDeleteDate = date_add($participant->created_at, date_interval_create_from_date_string($this->days . " days"));
-        $todayDate = date('Y-m-d', strtotime('now'));
 
         if($participant->competition_organization->competition->format === Competition::LOCAL){
+            $restrictDeleteDate = date_add($participant->created_at, date_interval_create_from_date_string($this->days . " days"));
+            $todayDate = date('Y-m-d', strtotime('now'));
             if($todayDate > $restrictDeleteDate) {
                 $fail('The selected participant could not be delete after 1 week of creation');
             }
