f4485f8cd4b637c81710c959cdb8ac733f627e28
Omar Elewa <omarelewa25@gmail.com>
2023-04-22 14:25:32 +0200

    edit the column name

--- database/migrations/2023_04_22_111125_competition_cheat_compute_status.php
+++ database/migrations/2023_04_22_111125_competition_cheat_compute_status.php
@@ -15,8 +15,8 @@ return new class extends Migration
     {
         Schema::create('competition_cheat_compute_status', function (Blueprint $table) {
             $table->foreignId('competition_id')->constrained('competition');
-            $table->enum('status', ['Not Started', 'In Progress', 'Completed', 'Bug Detected'])->default('Not Started');
-            $table->decimal('cheating_percentage', 3, 0);
+            $table->enum('status', ['In Progress', 'Completed', 'Failed'])->default('In Progress');
+            $table->decimal('progress_percentage', 3, 0);
             $table->string('compute_error_message')->nullable();
         });
     }
