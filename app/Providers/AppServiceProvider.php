7be93744826ebd858acdc88f7f55256f10b9906a
Omar Elewa <omarelewa25@gmail.com>
2023-04-24 09:50:51 +0200

    added collection methods to use for filter and pagination

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,11 @@
 
 namespace App\Providers;
 
+use App\Helpers\General\CollectionHelper;
+use Illuminate\Http\Request;
+use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Collection;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +28,27 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Collection::macro('filterByRequest', function (Request $request, $filters = [], $searchAttributes = []) {
+            $collection = $this;
+            if($request->hasAny($filters)){
+                CollectionHelper::adjustFilters($filters);
+                $collection = CollectionHelper::filterColletion($collection, $filters, $request);
+            }
+
+            if($request->has('search')){
+                $collection = CollectionHelper::searchInCollection($request->search, $collection, $searchAttributes);
+            }
+            return $collection;
+        });
+
+        Collection::macro('paginate', function ($perPage = 10, $page = 1, $options = []) {
+            $page = $page ?: (Paginator::resolveCurrentPage() ?: 1);
+            $results = $this->forPage($page, $perPage)->values();
+            $options += [
+                'path' => Paginator::resolveCurrentPath(),
+                'pageName' => 'page',
+            ];
+            return new LengthAwarePaginator($results, $this->count(), $perPage, $page, $options);
+        });
     }
 }
