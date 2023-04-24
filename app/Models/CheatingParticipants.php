fdc3980c446ce9903e87f0bc8c053cd11317ef27
Omar Elewa <omarelewa25@gmail.com>
2023-04-24 09:49:24 +0200

    models are added correctly

--- app/Models/CheatingParticipants.php
+++ app/Models/CheatingParticipants.php
@@ -8,4 +8,23 @@ use Illuminate\Database\Eloquent\Model;
 class CheatingParticipants extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+
+    public static $nextGroupId = 1;
+
+    public function participant()
+    {
+        return $this->belongsTo(Participants::class, 'participant_index', 'index_no');
+    }
+
+    public function otherParticipant()
+    {
+        return $this->belongsTo(Participants::class, 'cheating_with_participant_index', 'index_no');
+    }
+
+    public function competition()
+    {
+        return $this->belongsTo(Competition::class);
+    }
 }
