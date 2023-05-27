d6ec834c7e84af18fc9545dcbb946416b0adf916
Omar Elewa <omarelewa25@gmail.com>
2023-05-27 04:07:19 +0300

    make the view

---
+++ resources/views/upload-updated-answers-and-results.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Excel File Upload</title>
+</head>
+<body>
+    <h1>Excel File Upload</h1>
+    <form method="post" action="{{ route('upload-answer-and-results-csv') }}" enctype="multipart/form-data">
+        {{ csrf_field() }}
+        <input type="file" name="excel_file">
+        <button type="submit">Upload</button>
+    </form>
+</body>
+</html>
