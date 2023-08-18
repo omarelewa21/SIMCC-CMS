2c07089b5bbcb930f8f608746a22086a6652e7b5
abdelhakeemqassem7 <abdelhakeemqassem@gmail.com>
2023-08-18 08:20:56 +0300

    add new section validation to verified collection

--- app/Http/Requests/Collection/AddSectionRequest.php
+++ app/Http/Requests/Collection/AddSectionRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests\Collection;
 
+use App\Models\Collections;
 use App\Traits\CollectionAuthorizeRequestTrait;
 use Illuminate\Foundation\Http\FormRequest;
 
@@ -20,10 +21,25 @@ class AddSectionRequest extends FormRequest
             'collection_id'     => 'required|integer|exists:collection,id',
             'groups'            => 'required|array',
             'groups.*.task_id'  => 'array|required',
-            'groups.*.task_id.*' => 'required|integer',//exists:tasks,id
+            'groups.*.task_id.*' => 'required|integer', //exists:tasks,id
             'sort_randomly'     => 'boolean|required',
             'allow_skip'        => 'boolean|required',
             'description'       => 'string|max:65535',
         ];
+    }
+    /**
+     * Configure the validator instance.
+     *
+     * @param  \Illuminate\Validation\Validator  $validator
+     * @return void
+     */
+    public function withValidator($validator)
+    {
+        $collection = Collections::find($this->id);
+        $validator->after(function ($validator) use ($collection) {
+            if (!$collection->status == Collections::STATUS_VERIFIED) {
+                $validator->errors()->add('authorize', 'Collection is verified, Adding new sections is not allowed');
+            }
+        });
     }
 }
