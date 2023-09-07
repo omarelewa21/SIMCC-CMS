3c27122207bc600076e0819136c604de895902fc
Omar Elewa <omarelewa25@gmail.com>
2023-09-07 22:39:52 +0300

    add declartive types

--- app/Models/CompetitionRounds.php
+++ app/Models/CompetitionRounds.php
@@ -9,6 +9,9 @@ class CompetitionRounds extends Model
 {
     use HasFactory;
 
+    const AWARD_TYPE_POSITION = 1;
+    const AWARD_TYPE_PERCENTAGE = 0;
+
     Protected $table = 'competition_rounds';
     protected $guarded = [];
 
@@ -30,7 +33,7 @@ class CompetitionRounds extends Model
     public function getAwardTypeAttribute($value)
     {
         switch ($value) {
-            case 1:
+            case self::AWARD_TYPE_POSITION:
                 return 'Position';
                 break;
             default:
