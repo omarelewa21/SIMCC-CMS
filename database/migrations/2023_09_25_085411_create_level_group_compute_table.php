8ce8add2d63582aa4172852f705ff7874ce873f6
Omar Elewa <omarelewa25@gmail.com>
2023-09-27 19:09:19 +0300

    fix the migration

--- database/migrations/2023_09_25_085411_create_level_group_compute_table.php
+++ database/migrations/2023_09_25_085411_create_level_group_compute_table.php
@@ -17,7 +17,9 @@ return new class extends Migration
             $table->foreignId('level_id')->constrained('competition_levels')->onDelete('cascade');
             $table->foreignId('group_id')->constrained('competition_marking_group')->onDelete('cascade');
             $table->primary(['level_id', 'group_id']);
-            $table->enum('status', ['Not Started', 'In Progress', 'Finished', 'Bug Detected'])->default('Not Started');
+            $table->enum('computing_status', ['Not Started', 'In Progress', 'Finished', 'Bug Detected'])->default('Not Started');
+            $table->text('compute_error_message')->nullable();
+            $table->unsignedTinyInteger('compute_progress_percentage')->default(0);
         });
     }
 
