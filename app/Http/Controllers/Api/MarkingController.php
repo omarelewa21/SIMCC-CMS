dcb11cfe0fc084e44d6047eb24695f1e68137b91
Omar Elewa <omarelewa25@gmail.com>
2023-09-29 11:37:48 +0300

    ---

--- app/Http/Controllers/Api/MarkingController.php
+++ app/Http/Controllers/Api/MarkingController.php
@@ -317,10 +317,6 @@ class MarkingController extends Controller
                     foreach($competition->groups as $group){
                         if(ComputeLevelGroupService::validateLevelGroupForComputing($level, $group, false)) {
                             dispatch(new ComputeLevelGroupJob($level, $group, $request->all()));
-                            LevelGroupCompute::updateOrCreate(
-                                ['level_id' => $level->id, 'group_id' => $group->id],
-                                ['computing_status' => LevelGroupCompute::STATUS_IN_PROGRESS, 'compute_progress_percentage' => 1, 'compute_error_message' => null]
-                            );
                         }
                     }
                 }
