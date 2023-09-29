8193d67c283727654295d02065b9c0b109f806cf
Omar Elewa <omarelewa25@gmail.com>
2023-09-29 13:34:57 +0300

    fix migration and add primary key

--- database/migrations/2023_09_25_085411_create_level_group_compute_table.php
+++ database/migrations/2023_09_25_085411_create_level_group_compute_table.php
@@ -14,9 +14,9 @@ return new class extends Migration
     public function up()
     {
         Schema::create('level_group_compute', function (Blueprint $table) {
+            $table->id();
             $table->foreignId('level_id')->constrained('competition_levels')->onDelete('cascade');
             $table->foreignId('group_id')->constrained('competition_marking_group')->onDelete('cascade');
-            $table->primary(['level_id', 'group_id']);
             $table->enum('computing_status', ['Not Started', 'In Progress', 'Finished', 'Bug Detected'])->default('Not Started');
             $table->text('compute_error_message')->nullable();
             $table->unsignedTinyInteger('compute_progress_percentage')->default(0);
