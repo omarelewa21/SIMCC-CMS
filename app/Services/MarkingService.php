7665b54f33bd789e4abc2bbd89c02fb9e0f805f9
Omar Elewa <omarelewa25@gmail.com>
2023-11-03 08:40:03 +0300

    remove the condition for now

--- app/Services/MarkingService.php
+++ app/Services/MarkingService.php
@@ -98,11 +98,11 @@ class MarkingService
         $numberOfCorrectAnswersWithMarks = $level->taskMarks()->join('task_answers', function ($join) {
             $join->on('competition_tasks_mark.task_answers_id', 'task_answers.id')->whereNotNull('task_answers.answer');
         })->select('task_answers.task_id')->distinct()->count();
-
+        
         if($numberOfTasksIds === $numberOfCorrectAnswersWithMarks){
-            if($level->participantsAnswersUploaded()->count() > 0){
-                return $level->rounds->roundsAwards()->count() > 0;
-            }
+            // if($level->participantsAnswersUploaded()->count() > 0){
+            return $level->rounds->roundsAwards()->count() > 0;
+            // }
         };
 
         return false;
