1d7603fb67b9fea3276633d0c99f4d31fd0545cb
Omar Elewa <omarelewa25@gmail.com>
2023-11-23 11:16:20 +0300

    add controller

---
+++ app/Http/Controllers/Api/ParticipantAnswersController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\getParticipantListRequest;
+use App\Models\Competition;
+use App\Services\Competition\ParticipantAnswersListService;
+
+class ParticipantAnswersController extends Controller
+{
+    public function list(Competition $competition, getParticipantListRequest $request)
+    {
+        try {
+            $participantAnswersService = new ParticipantAnswersListService($competition, $request);
+            return response()->json([
+                "status"        => 200,
+                "message"       => "Success",
+                "filterOptions" => $participantAnswersService->getFilterOptions(),
+                "data"          => $participantAnswersService->getList()
+            ], 200);
+
+        } catch (\Exception $e) {
+            return response()->json([
+                "status"    => 500,
+                "message"   => "Internal Server Error {$e->getMessage()}",
+                "error"     => strval($e)
+            ], 500);
+        }
+    }
+}
