b1f0418049697815b7ace2d343125ba693936031
Omar Elewa <omarelewa25@gmail.com>
2023-11-23 11:48:19 +0300

    add delete method

--- app/Http/Controllers/Api/ParticipantAnswersController.php
+++ app/Http/Controllers/Api/ParticipantAnswersController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Competition\ParticipantAnswersDeleteRequest;
 use App\Http\Requests\getParticipantListRequest;
 use App\Models\Competition;
 use App\Services\Competition\ParticipantAnswersListService;
@@ -18,6 +19,25 @@ class ParticipantAnswersController extends Controller
                 "message"       => "Success",
                 "filterOptions" => $participantAnswersService->getFilterOptions(),
                 "data"          => $participantAnswersService->getList()
+            ], 200);
+
+        } catch (\Exception $e) {
+            return response()->json([
+                "status"    => 500,
+                "message"   => "Internal Server Error {$e->getMessage()}",
+                "error"     => strval($e)
+            ], 500);
+        }
+    }
+
+    public function delete(Competition $competition, ParticipantAnswersDeleteRequest $request)
+    {
+        try {
+            $participantAnswersService = new ParticipantAnswersListService($competition, $request);
+            $participantAnswersService->deleteParticipantsAnswers();
+            return response()->json([
+                "status"    => 200,
+                "message"   => "Success",
             ], 200);
 
         } catch (\Exception $e) {
