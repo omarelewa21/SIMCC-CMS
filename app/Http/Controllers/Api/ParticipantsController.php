7a85ac585a7aa273a76096f519abfcd930032ae4
abdelhakeemqassem7 <abdelhakeemqassem@gmail.com>
2023-11-27 15:55:42 +0200

    fix public_error message

--- app/Http/Controllers/Api/ParticipantsController.php
+++ app/Http/Controllers/Api/ParticipantsController.php
@@ -539,7 +539,7 @@ class ParticipantsController extends Controller
                     return response()->json([
                         'job_id' => $jobId,
                         'status' => ReportDownloadStatus::STATUS_FAILED,
-                        'message' => 'Failed to generate' . isset($report['public_error']) ? $report['public_error'] : '',
+                        'message' => 'Failed to generate' . (isset($report['public_error']) ? $report['public_error'] : ''),
                         'file_path' => '',
                         'progress' => $progress,
                     ], 200);
@@ -549,7 +549,7 @@ class ParticipantsController extends Controller
                         return response()->json([
                             'job_id' => $jobId,
                             'status' => ReportDownloadStatus::STATUS_FAILED,
-                            'message' => 'Failed to generate' . isset($report['public_error']) ? $report['public_error'] : '',
+                            'message' => 'Failed to generate' . (isset($report['public_error']) ? $report['public_error'] : ''),
                             'file_path' => '',
                             'progress' => 0,
                         ], 200);
