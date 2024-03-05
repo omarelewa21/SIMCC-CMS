21a6e8286449e693d0cea5d07b970e0b27415c31
Omar Elewa <omarelewa25@gmail.com>
2024-03-05 14:06:06 +0300

    remove level update

--- app/Http/Controllers/Api/MarkingController.php
+++ app/Http/Controllers/Api/MarkingController.php
@@ -298,7 +298,6 @@ class MarkingController extends Controller
             ], 200);
 
         } catch (\Exception $e) {
-            $level->updateStatus(CompetitionLevels::STATUS_BUG_DETECTED, $e->getMessage());
             return response()->json([
                 "status"    => $e->getCode() ?? 500,
                 "message"   => $e->getMessage() ?? "Level couldn't be computed",
