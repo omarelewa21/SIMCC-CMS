a4b2ba50d66d604d2790e71560942b1f315fc078
Omar Elewa <omarelewa25@gmail.com>
2024-03-19 10:54:14 +0200

    restrict access to edit school name

--- app/Http/Requests/UpdateSchoolRequest.php
+++ app/Http/Requests/UpdateSchoolRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests;
 
+use App\Models\CompetitionParticipantsResults;
 use App\Models\Roles;
 use App\Models\School;
 use App\Models\User;
@@ -68,10 +69,28 @@ class UpdateSchoolRequest extends FormRequest
                 if($school->country_id != $user->country_id) {
                     $validator->errors()->add('Permission Denied', "Only allowed to edit school's from current country");
                 }
+
+                if($this->schoolIsInvolvedInComputedComptitions()) {
+                    $validator->errors()->add('Permission Denied', "School is involved in computed competition, Please contact adminstrator to change its name");
+                
+                }
             }
             if ($user->hasRole(['Teacher', 'School Manager']) && $user->school_id != $school->id) {
                 $validator->errors()->add('Permission Denied', "Only allowed to edit own school");
             }
         });
+    }
+
+    /**
+     * Check if school is involved in computed competition
+     *
+     * @return bool
+     */
+    private function schoolIsInvolvedInComputedComptitions(): bool
+    {
+        return $this->filled('name')
+            && CompetitionParticipantsResults::whereRelation('participant', 'school_id', $this->id)
+                ->whereNotNull('global_rank')
+                ->exists();
     }
 }
