704bd1808c1b404569221b87635304f14bc00421
Omar Elewa <omarelewa25@gmail.com>
2024-04-12 18:26:29 +0200

    fixing the job file

--- app/Jobs/ComputeCheatingParticipants.php
+++ app/Jobs/ComputeCheatingParticipants.php
@@ -15,12 +15,6 @@ class ComputeCheatingParticipants implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
-    protected $competition;
-    protected $qNumber;         // If cheating question number >= $qNumber, then the participant is considered as cheater
-    protected $percentage;      // If cheating percentage >= $percentage, then the participant is considered as cheater
-    protected $number_of_incorrect_answers; // If number of incorrect answers > $number_of_incorrect_answers, then the participant is considered as cheater
-    protected $countries;       // If countries is not null, then only compute the cheating participants from these countries
-
     public $timeout = 5000;
 
     /**
@@ -29,19 +23,14 @@ class ComputeCheatingParticipants implements ShouldQueue
      * @return void
      */
     public function __construct(
-        Competition $competition,
-        $qNumber=null,
-        $percentage=null,
-        $number_of_incorrect_answers=null,
-        $countries=null
-    )
-    {
-        $this->competition = $competition;
-        $this->qNumber = $qNumber;
-        $this->percentage = $percentage;
-        $this->number_of_incorrect_answers = $number_of_incorrect_answers;
-        $this->countries = $countries;
-    }
+        protected Competition $competition,
+        protected $qNumber=null,
+        protected $percentage=null,
+        protected $number_of_incorrect_answers=null,
+        protected $countries=null,
+        protected $for_map_list = false
+
+    ) {}
 
     /**
      * Execute the job.
@@ -55,24 +44,24 @@ class ComputeCheatingParticipants implements ShouldQueue
                 $this->competition,
                 $this->qNumber, $this->percentage ?? 85,
                 $this->number_of_incorrect_answers ?? 5,
-                $this->countries
-            )
-            )->computeCheatingParticipants();
-
-        } catch (\Exception $e) {
-            CheatingStatus::updateOrCreate(
-                [
-                    'competition_id'    => $this->competition->id,
-                    'cheating_percentage'        => $this->percentage ?? 85,
-                    'number_of_same_incorrect_answers' => $this->number_of_incorrect_answers ?? 5,
-                    'countries'  => $this->countries ?? null,
-                ],
-                [
-                    'status' => 'Failed',
-                    'progress_percentage' => 0,
-                    'compute_error_message' => $e->getMessage()
-                ]
-            );
+                $this->countries,
+                $this->for_map_list ?? false
+            ))->computeCheatingParticipants();
+        }
+        
+        catch (\Exception $e) {
+            CheatingStatus::updateOrCreate([
+                'competition_id'                    => $this->competition->id,
+                'cheating_percentage'               => $this->percentage ?? 85,
+                'number_of_same_incorrect_answers'  => $this->number_of_incorrect_answers ?? 5,
+                'countries'                         => $this->countries ?? null,
+                'for_map_list'                      => $this->for_map_list
+            ],
+            [
+                'status'                => 'Failed',
+                'progress_percentage'   => 0,
+                'compute_error_message' => $e->getMessage()
+            ]);
         }
     }
 }
