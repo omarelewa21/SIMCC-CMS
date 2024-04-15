87c755ca2c9876d3c26a1f797433cc7bbdad13fe
Omar Elewa <omarelewa25@gmail.com>
2024-04-15 19:48:51 +0200

    fix validation message

--- app/Traits/IntegrityTrait.php
+++ app/Traits/IntegrityTrait.php
@@ -28,6 +28,7 @@ trait IntegrityTrait
             ->join('all_countries', 'all_countries.id', '=', 'competition_countries_for_integrity_check.country_id')
             ->where('competition_countries_for_integrity_check.is_confirmed', 1)
             ->select('all_countries.display_name as name')
+            ->distinct()
             ->get();
 
         if($countries->isEmpty()) return false;
