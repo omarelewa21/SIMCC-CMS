4c812221c8fc650d45eef4af4ca20b62aeca6d2c
Omar Elewa <omarelewa25@gmail.com>
2024-04-25 10:56:28 +0200

    insert records inside the summary

--- database/migrations/2024_04_25_083809_create_integrity_summaries_table.php
+++ database/migrations/2024_04_25_083809_create_integrity_summaries_table.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Models\CheatingStatus;
+use App\Models\IntegritySummary;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -21,6 +24,19 @@ return new class extends Migration
             $table->foreignId('run_by')->nullable()->constrained('users')->nullOnDelete();
             $table->timestamps();
         });
+
+        CheatingStatus::where('status', 'Completed')
+            ->get()
+            ->each(function ($status) {
+                DB::table('integrity_summaries')
+                    ->insert([
+                        'countries' => empty($status->original_countries) ? null : json_encode($status->original_countries),
+                        'total_cases_count' => $status->total_cases_count,
+                        'run_by' => $status->run_by,
+                        'created_at' => $status->created_at,
+                        'updated_at' => $status->updated_at,
+                    ]);
+            });
     }
 
     /**
