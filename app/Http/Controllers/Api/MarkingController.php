83031790fa408ccbc8c330d97938874b5e2e4615
Omar Elewa <omarelewa25@gmail.com>
2024-05-04 21:43:43 +0300

    --

--- app/Http/Controllers/Api/MarkingController.php
+++ app/Http/Controllers/Api/MarkingController.php
@@ -374,7 +374,14 @@ class MarkingController extends Controller
                 ->where('participants.status', '<>', Participants::STATUS_CHEATING)
                 ->exists();
 
-            throw_if($newAnswerUploadFound, new Exception('Please exit the Moderation Page and ensure all new students are marked first. You will need to complete this step before you can moderate the results again.'));
+            throw_if($newAnswerUploadFound, new Exception('Please exit the Moderation Page and ensure all new students are marked first. You will need to complete this step before you can moderate the results'));
+
+            $someAwardsNotMarked = CompetitionParticipantsResults::where('level_id', $level->id)
+                ->where('group_id', $group->id)
+                ->whereNull('award')
+                ->exists();
+
+            throw_if($someAwardsNotMarked, new Exception('Please exit the Moderation Page and ensure all students have awards marked. You will need to complete this step before you can moderate the results'));
 
             $level->load('rounds.competition', 'participantResults');
 
