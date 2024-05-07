e17d34be5e9f81c46c0a3b902d776910fa62ad51
Omar Elewa <omarelewa25@gmail.com>
2024-05-07 06:57:39 +0300

    add validation for moderating awards

--- app/Http/Requests/EditParticipantAwardRequest.php
+++ app/Http/Requests/EditParticipantAwardRequest.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Requests;
 
+use App\Models\CompetitionParticipantsResults;
+use App\Models\LevelGroupCompute;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Routing\Route;
 use Illuminate\Validation\Rule;
@@ -36,8 +38,28 @@ class EditParticipantAwardRequest extends FormRequest
         $awards->push($this->level->rounds->default_award_name);
         $awards->push("PERFECT SCORE");
         return [
-            '*.participant_index'   => 'required|exists:participants,index_no',
+            '*.participant_index'   => ['required', Rule::exists('competition_participants_results', 'participant_index')->where('level_id', $this->level->id)],
             '*.award'               => ['required', Rule::in($awards->toArray())]
         ];
+    }
+
+    /**
+     * Configure the validator instance.
+     *
+     * @param  \Illuminate\Validation\Validator  $validator
+     * @return void
+     */
+    public function withValidator($validator)
+    {
+        $validator->after(function ($validator) {
+            $firstIndex = $this['0']['participant_index'];
+            $groupId = CompetitionParticipantsResults::where('participant_index', $firstIndex)->value('group_id');
+            $levelGroupModerationCompleted = LevelGroupCompute::where('level_id', $this->level->id)
+            ->where('group_id', $groupId)->value('awards_moderated');
+
+            if($levelGroupModerationCompleted){
+                $validator->errors()->add('moderation', 'Moderation is completed, you need to revert moderation status to edit awards.');
+            }
+        });
     }
 }
