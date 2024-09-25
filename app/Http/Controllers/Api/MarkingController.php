cab3b9c3268888d0d64a86ee032accdecff63768
Omar Elewa <omarelewa25@gmail.com>
2024-09-25 09:55:33 +0300

    change moderation display

--- app/Http/Controllers/Api/MarkingController.php
+++ app/Http/Controllers/Api/MarkingController.php
@@ -425,7 +425,10 @@ class MarkingController extends Controller
             else{
                 $data = $level->participantResults()
                     ->where('competition_participants_results.group_id', $group->id)
-                    ->with('participant.school:id,name', 'participant.country:id,display_name as name')
+                    ->with(['participant' => fn ($query) => $query->join('grades', 'participants.grade', 'grades.id')
+                        ->select('participants.*', 'grades.display_name as grade')
+                        ->with(['school:id,name', 'country:id,display_name as name'])
+                    ])
                     ->orderBy('competition_participants_results.points', 'DESC')
                     ->orderBy('competition_participants_results.percentile', 'DESC')
                     ->get();
